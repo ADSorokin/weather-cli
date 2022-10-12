3393af0b9ee2cb431b25a456a73988060038104f
ADSorokin <alexsksd@gmail.com>
2022-10-12 15:02:16 +0300

    3 комит

--- weather.js
+++ weather.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
-import { printHelp } from './services/log.service.js';
+import { printHelp, printSuccess, printError } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+	try {
+		await saveKeyValue('token', token);
+		printSuccess('Токен сохранён');
+	} catch (e) {
+		printError(e.message);
+	}
+}
 
 const initCLI = () => {
 	const args = getArgs(process.argv)
@@ -14,6 +24,8 @@ const initCLI = () => {
 	}
 	if (args.t) {
 		// Сохранить токен
+		
+		return saveToken(args.t);
 	}
 	// Вывести погоду
 };
